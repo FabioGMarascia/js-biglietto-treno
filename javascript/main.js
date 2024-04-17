d68fcf50087b05e3c7febcfb71c0a64304d6b4ce
FabioGMarascia <fabiogaspare.marascia@gmail.com>
2024-04-17 16:33:48 +0200

    bonus

--- javascript/main.js
+++ javascript/main.js
@@ -1,17 +1,25 @@
 const km = prompt("Numero chilometri da percorrere");
 const anni = prompt("Età del passeggero");
-let prezzo_base = km * 0.21;
+const prezzo_base = km * 0.21;
 let sconto;
 let prezzo_scontato;
+let prezzo_arrotondato;
 
 if (anni < 18) {
   sconto = prezzo_base * 0.2;
   prezzo_scontato = prezzo_base - sconto;
-  console.log(prezzo_scontato);
+  prezzo_arrotondato = prezzo_scontato.toFixed(2);
+  console.log(prezzo_arrotondato);
 } else if (anni > 65) {
   sconto = prezzo_base * 0.4;
   prezzo_scontato = prezzo_base - sconto;
-  console.log(prezzo_scontato);
+  prezzo_arrotondato = prezzo_scontato.toFixed(2);
+  console.log(prezzo_arrotondato);
 } else {
-  console.log(prezzo_base);
+  prezzo_arrotondato = prezzo_base.toFixed(2);
+  console.log(prezzo_arrotondato);
 }
+
+document.getElementById(
+  "prezzo-biglietto"
+).innerHTML = `Il prezzo del biglietto è: ${prezzo_arrotondato}€`;
