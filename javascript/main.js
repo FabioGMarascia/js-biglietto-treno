ba1bb121d567b0cb47997c2200c22cdabea608a0
FabioGMarascia <fabiogaspare.marascia@gmail.com>
2024-04-18 11:23:00 +0200

    fix useless code 2

--- javascript/main.js
+++ javascript/main.js
@@ -1,21 +1,17 @@
 const km = prompt("Numero chilometri da percorrere");
 const anni = prompt("Età del passeggero");
 const prezzo_base = km * 0.21;
-let prezzo_scontato;
+let prezzo_scontato = prezzo_base;
 let prezzo_arrotondato;
 
 if (anni < 18) {
 	prezzo_scontato = prezzo_base * 0.8;
-	prezzo_arrotondato = prezzo_scontato.toFixed(2);
-	console.log(prezzo_arrotondato);
 } else if (anni > 65) {
 	prezzo_scontato = prezzo_base * 0.6;
-	prezzo_arrotondato = prezzo_scontato.toFixed(2);
-	console.log(prezzo_arrotondato);
-} else {
-	prezzo_arrotondato = prezzo_base.toFixed(2);
-	console.log(prezzo_arrotondato);
 }
+
+prezzo_arrotondato = prezzo_scontato.toFixed(2);
+console.log(prezzo_arrotondato);
 
 document.getElementById(
 	"prezzo-biglietto"
